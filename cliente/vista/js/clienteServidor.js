d7b699e826c80c428d3b526b4afe0b1fc8263de9
Alejandro Granados <alejandro@2nv.co>
2019-11-12 20:24:25 -0500

    anchor for functions in table

--- cliente/vista/js/clienteServidor.js
+++ cliente/vista/js/clienteServidor.js
@@ -3,6 +3,7 @@
  */
 
 const HOST = "http://api.clienteservidor.ue";
+const CLIENTE = "http://clienteservidor.ue"
 const HOST_SCRIPT = "servidor.php";
 
 function obtenerPersonas() {
@@ -20,7 +21,11 @@ function renderizarTablaPersonas(arregloPersonas){
             <td>${persona.primerNombre} ${persona.segundoNombre} ${persona.primerApellido} ${persona.segundoApellido}</td>
             <td>${persona.fechaNacimiento}</td>
             <td>${persona.correoElectronico}</td>
-            <td>Ver/Editar</td>
+            <td>
+              <a href="${CLIENTE}?p=detalle&id=${persona.id}">Ver</a> |
+              <a href="${CLIENTE}?p=editar&id=${persona.id}">Editar</a> |
+              <a href="${CLIENTE}?id=${persona.id}">Eliminar</a>
+            </td>
         </tr>`
     });
     nodoCuerpoTabla.html(personasHTML);
