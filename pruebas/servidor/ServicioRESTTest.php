f42d4988bbfcb418f46bf4ffa1b105683730f8b0
Sebastian <sgomez@belatrixsf.com>
2019-11-17 16:16:09 -0500

    Working first app test with phpunit

--- pruebas/servidor/ServicioRESTTest.php
+++ pruebas/servidor/ServicioRESTTest.php
@@ -1,11 +1,21 @@
 <?php
+use GuzzleHttp\Client;
 use PHPUnit\Framework\TestCase;
 
 class ServicioRESTTest extends TestCase
 {
+    protected $apiREST = 'http://api.clienteservidor.ue/servidor.php/';
+    protected $clienteHTTP;
+
+    public function setUp(): void
+    {
+        $this->clienteHTTP = new Client(['base_uri' => $this->apiREST]);
+    }
+
     public function testObtenerLista()
     {
-        $respuesta = $this->get('/');
-        $respuesta->assertStatus(200);
+        $respuesta = $this->clienteHTTP->request('GET', 'personas');
+        $codigoRespuesta = $respuesta->getStatusCode();
+        $this->assertEquals($codigoRespuesta, 200);
     }
 }
